52ab2464dd68d45dfaba5335e38175b6d840a50f
Colin Beckford <csb92@drexel.edu>
2018-10-10 12:49:25 -0400

    Adding in empty clause

--- clickerJudge.js
+++ clickerJudge.js
@@ -18,6 +18,12 @@ function loadClickScores() {
      };
      var getclickrequest = gapi.client.sheets.spreadsheets.values.batchGet(clickoutputparams);
      getclickrequest.then(function(response) {
+       if (jQuery.isEmptyObject(response) == true);
+       {
+         $('#click-player-name').html(playerList[0]);
+       }
+       else
+       {
        var clickoutput = (response.result.valueRanges[0].values);
        var majoroutput = (response.result.valueRanges[1].values);
        $('#click-player-name').html(playerList[clickoutput.length]);
@@ -45,6 +51,8 @@ function loadClickScores() {
      }, function(reason) {
        console.error('error: ' + reason.result.error.message);
      });
+      }
+
 }
 
 function updateClickEntry(i) {
