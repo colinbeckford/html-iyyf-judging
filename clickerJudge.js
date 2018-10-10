833e4a9eaacedc96417bc209fe09323242ba8e38
Colin Beckford <csb92@drexel.edu>
2018-10-10 13:09:29 -0400

    Adding more console logs

--- clickerJudge.js
+++ clickerJudge.js
@@ -19,9 +19,10 @@ function loadClickScores() {
      var getclickrequest = gapi.client.sheets.spreadsheets.values.batchGet(clickoutputparams);
      getclickrequest.then(function(response)
      {
+      console.log(jQuery.isEmptyObject(response));
        if (jQuery.isEmptyObject(response) == false)
        {
-      console.log(response);
+       console.log(response);
        var clickoutput = (response.result.valueRanges[0].values);
        var majoroutput = (response.result.valueRanges[1].values);
        $('#click-player-name').html(playerList[clickoutput.length]);
